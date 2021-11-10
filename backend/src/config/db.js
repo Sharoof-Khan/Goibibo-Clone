94269ca25d412113cbb55bd18949bfd89727f0b4
Sharoof-Khan <sharoofskhan10@gmail.com>
2021-11-10 11:30:36 +0530

    mongodb atlas Connection Updated with db name

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const connect = async() => {
 
-    return await mongoose.connect('mongodb+srv://goibibo:Srk123@goibibo.fpuqy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    return await mongoose.connect('mongodb+srv://goibibo:Srk123@goibibo.fpuqy.mongodb.net/goibibo?retryWrites=true&w=majority')
     
     
 }
